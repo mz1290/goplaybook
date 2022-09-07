dc2728915e50e8b31f5cc36f59de884f6e693c05
Michael Zimmerman <mzimme1290@gmail.com>
2022-09-07 08:01:47 -0500

    Linked list

--- linked_list.go
+++ linked_list.go
@@ -39,6 +39,20 @@ func DisplayLL(ptr *Node) {
 	fmt.Printf("\n")
 }
 
+func DisplayCycleLL(ptr *Node) {
+	head := ptr
+
+	for {
+		fmt.Printf("%d ", ptr.Data)
+		ptr = ptr.Next
+
+		if ptr == head {
+			break
+		}
+	}
+	fmt.Printf("\n")
+}
+
 func LenLL(ptr *Node) int {
 	count := 0
 
@@ -80,6 +94,48 @@ func InsertLL(ptr *Node, index, x int) *Node {
 	return head
 }
 
+func InsertCycleLL(p *Node, index, x int) *Node {
+	// Validate index
+	if index < 0 || index > FindCycleLength(p) {
+		return nil
+	}
+
+	head := p
+	temp := &Node{x, nil}
+
+	// Check inserting at head position
+	if index == 0 {
+		// Check if this is the first node
+		if head == nil {
+			head = temp
+
+			// Create cycle
+			head.Next = head
+		} else {
+			for p.Next != head {
+				p = p.Next
+			}
+
+			p.Next = temp
+			temp.Next = head
+			head = temp // optional
+		}
+	} else { // Thist matches inserting in linear linked list
+		// Stop at the node just before our desired index
+		for i := 0; i < index-1; i++ {
+			p = p.Next
+		}
+
+		// Copy node before position's next to new node about to be inserted
+		temp.Next = p.Next
+
+		// Update that same node's next to now be this inserted node
+		p.Next = temp
+	}
+
+	return head
+}
+
 func InsertSortedLL(ptr *Node, x int) *Node {
 	head := ptr
 	temp := &Node{x, nil}
@@ -107,4 +163,236 @@ func InsertSortedLL(ptr *Node, x int) *Node {
 	}
 
 	return head
+}
+
+func DeleteNodeLL(ptr *Node, index int) *Node {
+	head := ptr
+	prev := head
+
+	if index < 0 || index > LenLL(head) {
+		return head
+	}
+
+	if index == 0 {
+		return head.Next
+	}
+
+	for i := 0; i < index; i++ {
+		prev = ptr
+		ptr = ptr.Next
+	}
+
+	prev.Next = ptr.Next
+	// Node at ptr has now been deleted from list GC will clean up
+
+	return head
+}
+
+func DeleteNodeCycleLL(p *Node, idx int) *Node {
+	head := p
+	prev := head
+
+	// Validate index
+	if idx < 0 || idx > FindCycleLength(p)-1 {
+		return nil
+	}
+
+	if idx == 0 {
+		for p.Next != head {
+			p = p.Next
+		}
+
+		if p == head {
+			// We only have one node left, delete
+			return nil
+		} else {
+			// Update tail to point to new head
+			p.Next = head.Next
+
+			// Delete head by simply returning it's next value
+			return head.Next
+		}
+	}
+
+	// Otherwise treat as you normally would linear LL delete
+	for i := 0; i < idx; i++ {
+		prev = p
+		p = p.Next
+	}
+
+	prev.Next = p.Next
+
+	return head
+}
+
+func IsLinkedListSorted(ptr *Node) bool {
+	prev := ptr.Data
+	ptr = ptr.Next
+
+	for ptr != nil {
+		if ptr.Data < prev {
+			return false
+		}
+
+		prev = ptr.Data
+		ptr = ptr.Next
+	}
+
+	return true
+}
+
+// Linked list must be sorted
+func RemoveDuplicatesLL(ptr *Node) *Node {
+	head := ptr
+	next := ptr.Next
+
+	for next != nil {
+		if ptr.Data != next.Data {
+			ptr = next
+			next = next.Next
+		} else {
+			// Remove duplicate
+			ptr.Next = next.Next
+			// node pointed to by 'next' is now removed
+
+			next = ptr.Next
+		}
+	}
+
+	return head
+}
+
+func reverseLL(head *Node) *Node {
+	// Sliding pointers
+	current := head
+	var prev *Node = nil
+
+	for current != nil {
+		// Store current's next for later assignment
+		next := current.Next
+
+		// Peform the link reversal
+		current.Next = prev
+
+		// Current is the next rounds previous
+		prev = current
+
+		// Current is the previously saved next from above
+		current = next
+	}
+
+	// Previous will hold the new head when complete
+	return prev
+}
+
+func reverseLLR(head *Node) *Node {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	r := reverseLLR(head.Next)
+
+	// Reverse the links on the recursive return by updating the updated list
+	// with the new node and bubbling up the nil until reaching the original
+	// first node
+	head.Next.Next = head
+	head.Next = nil
+	return r
+}
+
+// Assumes both lists are sorted
+func mergeLL(first, second *Node) *Node {
+	var dummy = new(Node)
+
+	// p serves as our LL iterator. It will take the lower node from the two
+	// lists and update the previously appeneded node's next to point to new
+	// node.
+	p := dummy
+
+	for first != nil && second != nil {
+		if first.Data < second.Data {
+			// The first value is smaller so attach to dummy list ahead
+			p.Next = first
+
+			// Advance to the following node in first list
+			first = first.Next
+		} else {
+			p.Next = second
+			second = second.Next
+		}
+
+		// Now that we've added a new node, advance to it so that we may update
+		// it's "next" pointer
+		p = p.Next
+	}
+
+	// Check if there are any trailing nodes and append
+	if first != nil {
+		p.Next = first
+	} else {
+		p.Next = second
+	}
+
+	return dummy.Next
+}
+
+func hasCycleLL(head *Node) bool {
+	slow := head
+	fast := head
+
+	for fast != nil && fast.Next != nil {
+		/* Advance fast 2 nodes */
+		fast = fast.Next.Next
+
+		/* Advance slow 1 node */
+		slow = slow.Next
+
+		/* Check if pointers have met */
+		if slow == fast {
+			return true
+		}
+	}
+
+	return false
+}
+
+// This also determines if list has cycle
+func FindCycleLength(head *Node) int {
+	slow := head
+	fast := head
+
+	for fast != nil && fast.Next != nil {
+		/* Advance fast 2 nodes */
+		fast = fast.Next.Next
+
+		/* Advance slow 1 node */
+		slow = slow.Next
+
+		/* Check if pointers have met */
+		if slow == fast {
+			return CalculateCycleLength(slow)
+		}
+	}
+
+	return 0
+}
+
+func CalculateCycleLength(slow *Node) int {
+	current := slow
+	cycleLen := 0
+
+	for {
+		/* Get next node */
+		current = current.Next
+
+		/* Increment length */
+		cycleLen++
+
+		/* Check if we've circled back */
+		if current == slow {
+			break
+		}
+	}
+
+	return cycleLen
 }
--- linked_list_test.go
+++ linked_list_test.go
@@ -50,3 +50,95 @@ func TestInsertSortedLL(t *testing.T) {
 	head = InsertSortedLL(nil, 5)
 	DisplayLL(head)
 }
+
+func TestDeleteNodeLL(t *testing.T) {
+	test := []int{10, 20, 30, 40, 50}
+	head := CreateLL(test)
+
+	head = DeleteNodeLL(head, 1)
+	DisplayLL(head)
+
+	head = DeleteNodeLL(head, 0)
+	DisplayLL(head)
+}
+
+func TestIsLinkedListSorted(t *testing.T) {
+	test := []int{10, 20, 30, 40, 50}
+	head := CreateLL(test)
+	fmt.Println(IsLinkedListSorted(head)) // true
+
+	head = InsertLL(head, 2, 100)
+	fmt.Println(IsLinkedListSorted(head)) // false
+}
+
+func TestRemoveDuplicatesLL(t *testing.T) {
+	test := []int{10, 20, 20, 20, 30, 40, 40, 40, 50}
+	head := CreateLL(test)
+
+	head = RemoveDuplicatesLL(head)
+	DisplayLL(head)
+}
+
+func TestReverseLL(t *testing.T) {
+	test := []int{10, 20, 30, 40, 50}
+	head := CreateLL(test)
+
+	fmt.Println("reversing links...")
+	head = reverseLL(head)
+	DisplayLL(head)
+
+	// Reset LL
+	head = CreateLL(test)
+	fmt.Println("reversing links recursively...")
+	head = reverseLLR(head)
+	DisplayLL(head)
+}
+
+func TestMergeLL(t *testing.T) {
+	test := []int{10, 20, 30, 40, 50}
+	test2 := []int{5, 15, 25, 35, 45}
+	first := CreateLL(test)
+	second := CreateLL(test2)
+
+	head := mergeLL(first, second)
+	DisplayLL(head)
+}
+
+func TestHasCycleLL(t *testing.T) {
+	test := []int{10, 20, 30, 40, 50}
+	head := CreateLL(test)
+
+	t1 := head.Next.Next
+	t2 := head.Next.Next.Next.Next
+	t2.Next = t1
+
+	fmt.Println(hasCycleLL(head)) // true
+}
+
+func TestInsertCycleLL(t *testing.T) {
+	test := []int{2, 3, 4, 5, 6}
+	head := CreateLL(test)
+
+	t1 := head.Next.Next.Next.Next
+	t1.Next = head
+
+	head = InsertCycleLL(head, 5, 10)
+	if head == nil {
+		t.Fatal("got nil head")
+	}
+	DisplayCycleLL(head)
+}
+
+func TestDeleteCycleLL(t *testing.T) {
+	test := []int{2, 3, 4, 5, 6}
+	head := CreateLL(test)
+
+	t1 := head.Next.Next.Next.Next
+	t1.Next = head
+
+	head = DeleteNodeCycleLL(head, 4)
+	if head == nil {
+		t.Fatal("received nil head")
+	}
+	DisplayCycleLL(head)
+}
